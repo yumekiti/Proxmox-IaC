1d9ad63cd1edd70cd4558522aec7c53865fd78ea
yumekiti <yumekiti120440@gmail.com>
2023-12-25 05:34:39 +0900

    chore: add jenkins

--- main.tf
+++ main.tf
@@ -1,7 +1,17 @@
-# lxc-basic
 module "lxc-basic" {
   hostname         = "lxc-basic"
   source           = "./lxc-basic/terraform"
+  target_node      = "node1"
+  bridge_name      = "vmbr1"
+  ipv4_address     = "10.10.10.10/24"
+  gateway_address  = "10.10.10.1"
+  public_key_path  = var.public_key_path
+  private_key_path = var.private_key_path
+}
+
+module "jenkins" {
+  hostname         = "jenkins"
+  source           = "./jenkins/terraform"
   target_node      = "node1"
   bridge_name      = "vmbr1"
   ipv4_address     = "10.10.10.10/24"
