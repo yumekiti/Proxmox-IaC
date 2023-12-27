e48596c55d804ed073d93628fb17258b16878d68
yumekiti <yumekiti120440@gmail.com>
2023-12-27 20:50:05 +0900

    fix: remote-execを使わないように変更

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 module "lxc-basic" {
   hostname         = "lxc-basic"
   source           = "./lxc-basic/terraform"
-  target_node      = "proxmox"
-  bridge_name      = "vmbr0"
-  ipv4_address     = "192.168.11.30/24"
-  gateway_address  = "192.168.11.1"
+  target_node      = "node1"
+  bridge_name      = "vmbr1"
+  ipv4_address     = "10.10.10.10/24"
+  gateway_address  = "10.10.10.1"
   public_key_path  = var.public_key_path
   private_key_path = var.private_key_path
 }
